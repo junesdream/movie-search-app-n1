52c9d25cbfb7115ba249b9a58fd1104eb452b375
June <norisori@gmail.com>
2022-02-02 18:35:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [end, setEnd] = useState("");
+	const [film, setFilm] = useState([]);
+	const inputField = useRef("");
+
+	useEffect(() => {
+		fetch(
+			`https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/+${end}`,
+			{
+				method: "GET",
+				headers: {
+					"x-rapidapi-host":
+						"imdb-internet-movie-database-unofficial.p.rapidapi.com",
+					"x-rapidapi-key":
+						"2199f9d855msh1187bcf1a59ad6bp10b524jsn334cf16c9851",
+				},
+			}
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				setFilm(data.titles);
+				// console.log(data.titles)
+			})
+
+			.catch((err) => {
+				console.error(err);
+			});
+	}, [end]);
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		setEnd(inputField.current.value);
+	};
+
+	return (
+		<div className="App">
+			<form onSubmit={onSubmit}>
+				<input type="text" placeholder="Type the name of movies..." ref={inputField} />
+				<button className="submit">Submit</button>
+			</form>
+
+			<div className="print">
+				{film.map((item, index) => {
+					return (
+						<div key={index} className="container">
+							<img src={item.image} alt="" />
+							<p>{item.title}</p>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
